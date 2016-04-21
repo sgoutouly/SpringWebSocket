22ee5da0804e3aa4a7a03b0926ca883c968494de
sgoutouly <sgoutouly@gmail.com>
2016-04-21 23:43:49 +0200

    nettoyage du code Javascript et ajout de commentaire pour la partie Java

--- src/main/java/com/sylvaingoutouly/spring/websocket/config/BrokerWebSocketConfig.java
+++ src/main/java/com/sylvaingoutouly/spring/websocket/config/BrokerWebSocketConfig.java
@@ -10,7 +10,7 @@ import java.util.logging.Logger;
 
 /**
  * Configuration de WebSocket avec STOMP. Implique l'usage d'un broker pour router
- * les message vers des destinataires différents (clients connectés, contrôleur ou autre)
+ * les message vers des destinataires différents (clients connectés, contrôleur applicatif ou autre)
  * en fonction d'un entête de type destinataire placé dans l'enveloppe STOMP
  * Ici on se contentra de router les messages vers les clients connectés
  */
