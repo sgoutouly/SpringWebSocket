b413512ba961f2ae47489940aa0e6aad89c61f1b
sgoutouly <sgoutouly@gmail.com>
2016-04-24 19:23:07 +0200

    Add support for Multiple concurrent SSE

--- src/main/java/com/sylvaingoutouly/spring/websocket/api/SSEController.java
+++ src/main/java/com/sylvaingoutouly/spring/websocket/api/SSEController.java
@@ -7,26 +7,40 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 /**
  * Created by sylvain on 23/04/2016.
+ *
+ * @see Sebastien Deleuze Github : https://github.com/sdeleuze/spring-react-isomorphic/blob/master/src/main/java/io/spring/isomorphic/CommentController.java
  */
 @RestController
 @RequestMapping(value = "/sse")
 public class SseController {
 
-    @Autowired
-    FirstSse sse;
+    /** Hold all SseEmitter instances */
+    private List<SseEmitter> sseEmitters = Collections.synchronizedList(new ArrayList<SseEmitter>());
 
     /**
      * EndPoint de connection au SSE
      * @return SseEmitter Le stream
      */
-    @RequestMapping(value = "/connect", method = GET)
-    public SseEmitter stream() {
-        return sse;
+    @RequestMapping("/connect")
+    public SseEmitter connect() {
+        SseEmitter sseEmitter = new SseEmitter();
+        synchronized (this.sseEmitters) { // Need to synchronize the list when you iterate it
+            this.sseEmitters.add(sseEmitter);
+            sseEmitter.onCompletion(() -> {
+                synchronized (this.sseEmitters) {
+                    this.sseEmitters.remove(sseEmitter);
+                }
+            });
+        }
+        return sseEmitter;
     }
 
     /**
@@ -34,9 +48,17 @@ public class SseController {
      * @throws IOException
      */
     @RequestMapping(value = "/write", method = GET)
-    public void write() throws IOException {
-        sse.send("coucou");
-        //sse.complete();
+    public void write() {
+        synchronized (this.sseEmitters) { // Need to synchronize the list when you iterate it
+            for (SseEmitter sseEmitter : this.sseEmitters) {
+                // Servlet containers don't always detect ghost connection, so we must catch exceptions ...
+                // Is it always needed ?
+                try {
+                    sseEmitter.send("coucou");
+                } catch (Exception e) {}
+            }
+        }
+
     }
 
 }
